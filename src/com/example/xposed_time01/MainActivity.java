ef8d42845433748fa6867c55c8c1a0eb617dad3f
Json Lee <jiansongli426@gmail.com>
2016-08-19 11:10:40 +0800

    json lee

---
+++ src/com/example/xposed_time01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xposed_time01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_main);	
+	}
+}
